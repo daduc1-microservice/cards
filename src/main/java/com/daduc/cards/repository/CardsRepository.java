34079981bd02325010d81fda47e98a1c3a30040a
dauduc2502 <dauduc2502@gmail.com>
2024-08-09 22:39:34 +0700

    [ADD] Add Cards Microservice

---
+++ src/main/java/com/daduc/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.daduc.cards.repository;
+
+import com.daduc.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
